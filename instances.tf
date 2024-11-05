0262f8cb3a47bd51fa3d1f46060c46c0d9d57082
0xPhantasia <evan.mestre02@gmail.com>
2024-11-05 10:07:40 +0100

    Addition of SG, EC2 and lookup file

---
+++ instances.tf
@@ -0,0 +1,51 @@
+# Retrieve the latest Ubuntu 20.04 LTS AMI
+data "aws_ami" "ubuntu_latest" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-*-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"]
+}
+
+
+#Create Nextcloud VM
+resource "aws_instance" "nextcloud" {
+  ami = data.aws_ami.ubuntu_latest.id
+  instance_type = "t3.micro"
+  subnet_id = aws_subnet.private_subnets.id
+  security_groups = [aws_security_group.nextcloud-sg.name]
+#  network_interface {
+#    device_index = 0
+#    network_interface_id = aws_subnet.private_subnets[0]
+#  }
+
+  tags = {
+    Name = "local.name"
+    Owner = "local.user"
+  }
+}
+
+#Create Bastion VM
+resource "aws_instance" "bastion" {
+  ami = "ami-00d81861317c2cc1f"
+  instance_type = "t3.micro"
+  subnet_id = aws_subnet.public_subnets.id
+  security_groups = [aws_security_group.bastion-sg.name]
+#  network_interface {
+#    device_index = 0
+#    network_interface_id = aws_subnet.public_subnets[0]
+#  }
+
+  tags = {
+    Name = "local.name"
+    Owner = "local.user"
+  }
+}
---
+++ security.tf
@@ -0,0 +1,45 @@
+# Bastion Security Group
+resource "aws_security_group" "bastion-sg" {
+  vpc_id = aws_vpc.vpc.id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["10.0.0.0/16"]  # Restrict SSH access to within VPC
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = local.name
+  }
+}
+
+# Nextcloud Security Group
+resource "aws_security_group" "nextcloud-sg" {
+  vpc_id = aws_vpc.vpc.id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["10.0.0.0/16"]  # Restrict SSH access to within VPC
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = local.name
+  }
+}
