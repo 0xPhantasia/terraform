becbe4c39224a9cb44f97f9d230c32b132256164
0xPhantasia <evan.mestre02@gmail.com>
2024-11-05 10:54:48 +0100

    SG

--- security.tf
+++ security.tf
@@ -2,44 +2,72 @@
 resource "aws_security_group" "bastion-sg" {
   vpc_id = aws_vpc.vpc.id
 
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["10.0.0.0/16"]  # Restrict SSH access to within VPC
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+#  ingress {
+#    from_port   = 22
+#    to_port     = 22
+#    protocol    = "tcp"
+#    cidr_blocks = ["10.0.0.0/16"]  # Restrict SSH access to within VPC
+#  }
+#
+#  egress {
+#    from_port   = 0
+#    to_port     = 0
+#    protocol    = "-1"
+#    cidr_blocks = ["0.0.0.0/0"]
+#  }
 
   tags = {
     Name = local.name
   }
 }
 
+resource "aws_vpc_security_group_ingress_rule" "allow_ssh_ipv4_in" {
+  security_group_id = aws_security_group.bastion-sg.id
+  cidr_ipv4 = aws_vpc.vpc.cidr_block
+  from_port = 22
+  ip_protocol = "tcp"
+  to_port = 22
+}
+
+resource "aws_vpc_security_group_egress_rule" "allow_ssh_ipv4_out" {
+  security_group_id = aws_security_group.bastion-sg.id
+  cidr_ipv4         = "0.0.0.0/0" #Mettre IP dynamique instance Nextcloud
+  ip_protocol       = "-1" #Mettre SSH 
+}
+
 # Nextcloud Security Group
 resource "aws_security_group" "nextcloud-sg" {
   vpc_id = aws_vpc.vpc.id
 
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["10.0.0.0/16"]  # Restrict SSH access to within VPC
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+#  ingress {
+#    from_port   = 22
+#    to_port     = 22
+#    protocol    = "tcp"
+#    cidr_blocks = ["10.0.0.0/16"]  # Restrict SSH access to within VPC
+#  }
+#
+#  egress {
+#    from_port   = 0
+#    to_port     = 0
+#    protocol    = "-1"
+#    cidr_blocks = ["0.0.0.0/0"]
+#  }
 
   tags = {
     Name = local.name
   }
+}
+
+resource "aws_vpc_security_group_ingress_rule" "allow_ssh_ipv4_in" {
+  security_group_id = aws_security_group.nextcloud-sg.id
+  cidr_ipv4 = aws_vpc.vpc.cidr_block
+  from_port = 22
+  ip_protocol = "tcp"
+  to_port = 22
+}
+
+resource "aws_vpc_security_group_egress_rule" "allow_ssh_ipv4_out" {
+  security_group_id = aws_security_group.nextcloud-sg.id
+  cidr_ipv4         = "0.0.0.0/0" #Vérifier
+  ip_protocol       = "-1" #Vérifier
 }
