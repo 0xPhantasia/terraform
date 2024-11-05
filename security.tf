f1edff06d7a59da042360a7308350d5cfd268d6d
0xPhantasia <evan.mestre02@gmail.com>
2024-11-05 15:06:29 +0100

    SSH

--- security.tf
+++ security.tf
@@ -28,7 +28,9 @@ resource "aws_vpc_security_group_ingress_rule" "allow_bastion_ssh_ipv4_in" {
 resource "aws_vpc_security_group_egress_rule" "allow_bastion_ssh_ipv4_out" {
   security_group_id = aws_security_group.bastion-sg.id
   cidr_ipv4         = "${aws_instance.nextcloud.private_ip}/32"
+  from_port         = 22
   ip_protocol       = "tcp"
+  to_port           = 22
 }
 
 # Nextcloud Security Group
