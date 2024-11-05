6de057fb930fac283a45f1b8a463762d9fba257b
0xPhantasia <evan.mestre02@gmail.com>
2024-11-05 11:18:37 +0100

    EC2

--- instances.tf
+++ instances.tf
@@ -18,7 +18,7 @@ data "aws_ami" "ubuntu_latest" {
 
 #Create Nextcloud VM
 resource "aws_instance" "nextcloud" {
-  ami = [data.aws_ami.ubuntu_latest.id]
+  ami = data.aws_ami.ubuntu_latest.id
   instance_type = "t3.micro"
   subnet_id = aws_subnet.private_subnets[keys(aws_subnet.private_subnets)[0]].id
   vpc_security_group_ids = [aws_security_group.nextcloud-sg.id]
@@ -35,7 +35,7 @@ resource "aws_instance" "nextcloud" {
 
 #Create Bastion VM
 resource "aws_instance" "bastion" {
-  ami = "ami-00d81861317c2cc1f"
+  ami = data.aws_ami.ubuntu_latest.id
   instance_type = "t3.micro"
   subnet_id = aws_subnet.public_subnets[keys(aws_subnet.public_subnets)[0]].id
   vpc_security_group_ids = [aws_security_group.bastion-sg.id]
