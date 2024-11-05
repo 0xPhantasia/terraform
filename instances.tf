5c985b2fe29f7b886bef8b331c54b5a37d196f80
0xPhantasia <evan.mestre02@gmail.com>
2024-11-05 11:12:54 +0100

    EC2

--- instances.tf
+++ instances.tf
@@ -21,7 +21,7 @@ resource "aws_instance" "nextcloud" {
   ami = data.aws_ami.ubuntu_latest.id
   instance_type = "t3.micro"
   subnet_id = aws_subnet.private_subnets[keys(aws_subnet.private_subnets)[0]].id
-  vpc_security_group_ids = aws_security_group.nextcloud-sg.id
+  vpc_security_group_ids = [aws_security_group.nextcloud-sg.id]
 #  network_interface {
 #    device_index = 0
 #    network_interface_id = aws_subnet.private_subnets[0]
@@ -38,7 +38,7 @@ resource "aws_instance" "bastion" {
   ami = "ami-00d81861317c2cc1f"
   instance_type = "t3.micro"
   subnet_id = aws_subnet.public_subnets[keys(aws_subnet.public_subnets)[0]].id
-  vpc_security_group_ids = aws_security_group.bastion-sg.id
+  vpc_security_group_ids = [aws_security_group.bastion-sg.id]
 #  network_interface {
 #    device_index = 0
 #    network_interface_id = aws_subnet.public_subnets[0]
