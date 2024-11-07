30333a826b01b39e372c1cff101e1e0aceec3084
0xPhantasia <evan.mestre02@gmail.com>
2024-11-07 12:10:14 +0100

    EFS

--- tp05-ex01/efs.tf
+++ tp05-ex01/efs.tf
@@ -14,9 +14,11 @@ resource "aws_efs_file_system" "nextcloud-fs" {
 }
 
 resource "aws_efs_mount_target" "nextcloud-fs-mounts" {
-  count          = length(aws_subnet.private_subnets.id)
+#  count          = length(aws_subnet.private_subnets.id)
   file_system_id = aws_efs_file_system.nextcloud-fs.id
-  subnet_id      = element(aws_subnet.private_subnets.id[each.key], count.index)
+#  subnet_id      = element(aws_subnet.private_subnets.id, count.index)
+  for_each = aws_subnet.private_subnets.id
+  subnet_id      = each.value
   security_groups = [
     aws_security_group.efs-sg.id
   ]
