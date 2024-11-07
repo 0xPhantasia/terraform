fe0ba33716d346aec2a03d3d3b3715dd8e945b8f
0xPhantasia <evan.mestre02@gmail.com>
2024-11-07 11:56:41 +0100

    EFS network conf

--- tp05-ex01/efs.tf
+++ tp05-ex01/efs.tf
@@ -16,7 +16,7 @@ resource "aws_efs_file_system" "nextcloud-fs" {
 resource "aws_efs_mount_target" "nextcloud-fs-mounts" {
   count          = length(aws.private_subnets) # Assumes 3 subnets, one in each AZ
   file_system_id = aws_efs_file_system.nextcloud-fs.id
-  subnet_id      = element(aws.private_subnets.id, count.index)
+  subnet_id      = element(aws_subnet.private_subnets, count.index)
   security_groups = [
     aws_security_group.efs-sg.id
   ]
