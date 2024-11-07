4686592fd1f82dd075bfdd5f53998a90e3dd3eb9
0xPhantasia <evan.mestre02@gmail.com>
2024-11-07 16:34:43 +0100

    Correction of duplicate rule

--- tp05-ex01/security.tf
+++ tp05-ex01/security.tf
@@ -49,21 +49,10 @@ resource "aws_security_group" "nextcloud-sg" {
 #  to_port           = 22
 #}
 
-resource "aws_vpc_security_group_ingress_rule" "allow_nextcloud_ssh_ipv4_in" {
+resource "aws_vpc_security_group_egress_rule" "allow_nextcloud_all_ipv4_out" {
   security_group_id = aws_security_group.nextcloud-sg.id
   cidr_ipv4         = "0.0.0.0/0"
-  from_port         = 22
   ip_protocol       = "-1"
-  to_port           = 22
-}
-
-resource "aws_security_group_rule" "allow_nextcloud_all_ipv4_out" {
-  type              = "egress"
-  from_port         = 0
-  to_port           = 0
-  protocol          = "-1"
-  cidr_blocks       = ["0.0.0.0/0"]
-  security_group_id = aws_security_group.nextcloud-sg.id
 }
 
 # EFS Security Group
