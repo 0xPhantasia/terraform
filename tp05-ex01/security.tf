cbf0ba57a6d1eb893be9cc3cd571045368adec36
0xPhantasia <evan.mestre02@gmail.com>
2024-11-07 16:32:36 +0100

    Removing nextcloud restrictions

--- tp05-ex01/security.tf
+++ tp05-ex01/security.tf
@@ -57,10 +57,13 @@ resource "aws_vpc_security_group_ingress_rule" "allow_nextcloud_ssh_ipv4_in" {
   to_port           = 22
 }
 
-resource "aws_vpc_security_group_egress_rule" "allow_nextcloud_all_ipv4_out" {
+resource "aws_security_group_rule" "allow_nextcloud_all_ipv4_out" {
+  type              = "egress"
+  from_port         = 0
+  to_port           = 0
+  protocol          = "-1"
+  cidr_blocks       = ["0.0.0.0/0"]
   security_group_id = aws_security_group.nextcloud-sg.id
-  cidr_ipv4         = "0.0.0.0/0"
-  ip_protocol       = "-1"
 }
 
 # EFS Security Group
