cde3308e8efacbc3f97b92e69a9d618cab74b34b
0xPhantasia <evan.mestre02@gmail.com>
2024-11-07 16:36:54 +0100

    Rule edition

--- tp05-ex01/security.tf
+++ tp05-ex01/security.tf
@@ -49,6 +49,14 @@ resource "aws_security_group" "nextcloud-sg" {
 #  to_port           = 22
 #}
 
+resource "aws_vpc_security_group_ingress_rule" "allow_nextcloud_ssh_ipv4_in" {
+  security_group_id = aws_security_group.nextcloud-sg.id
+  cidr_ipv4         = "0.0.0.0/0"
+  from_port         = 0
+  ip_protocol       = "-1"
+  to_port           = 0
+}
+
 resource "aws_vpc_security_group_egress_rule" "allow_nextcloud_all_ipv4_out" {
   security_group_id = aws_security_group.nextcloud-sg.id
   cidr_ipv4         = "0.0.0.0/0"
