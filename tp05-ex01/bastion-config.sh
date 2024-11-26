2dd41291e7284ae0b6d6465f13e158f6e2fc202c
0xPhantasia <evan.mestre02@gmail.com>
2024-11-26 14:55:42 +0100

    Changing private to public ip

--- tp05-ex01/bastion-config.sh
+++ tp05-ex01/bastion-config.sh
@@ -1,3 +1,3 @@
 # Insecure, use for debugging only
 scp -i ${path.module}/nextcloud-ssh.pem /home/ubuntu/nextcloud-ssh.pem \
-ubuntu@${aws_instance.bastion.private_ip}:/home/ubuntu/
+ubuntu@${aws_instance.bastion.public_ip}:/home/ubuntu/
