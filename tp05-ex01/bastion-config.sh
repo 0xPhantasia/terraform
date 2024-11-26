760bc7f477c1f1b71387a1051e6e92cce4578e1f
0xPhantasia <evan.mestre02@gmail.com>
2024-11-26 15:34:17 +0100

    Commenting out bastion user_data instead of nextcloud

--- tp05-ex01/bastion-config.sh
+++ tp05-ex01/bastion-config.sh
@@ -1,3 +1,5 @@
 # Insecure, use for debugging only
 scp -i ${path.module}/nextcloud-ssh.pem /home/ubuntu/nextcloud-ssh.pem \
 ubuntu@${aws_instance.bastion.public_ip}:/home/ubuntu/
+
+# scp -i ./bastion-ssh.pem $PWD/nextcloud-ssh.pem ubuntu@13.48.26.123:/home/ubuntu/nextcloud-ssh.pem
