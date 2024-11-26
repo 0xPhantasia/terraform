4c9870c74f70bc50098762072b7ba997c14fab70
0xPhantasia <evan.mestre02@gmail.com>
2024-11-26 14:52:16 +0100

    Removing destination filename

--- tp05-ex01/bastion-config.sh
+++ tp05-ex01/bastion-config.sh
@@ -1,3 +1,3 @@
 # Insecure, use for debugging only
 scp -i ${path.module}/nextcloud-ssh.pem /home/ubuntu/nextcloud-ssh.pem \
-ubuntu@${aws_instance.bastion.private_ip}:/home/ubuntu/nextcloud-ssh.pem
+ubuntu@${aws_instance.bastion.private_ip}:/home/ubuntu/
