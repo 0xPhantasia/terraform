4f2236b3b3072cf7fcdfff0dd7d1b8df8e300318
0xPhantasia <evan.mestre02@gmail.com>
2024-11-26 12:05:05 +0100

    do

--- tp05-ex01/outputs.tf
+++ tp05-ex01/outputs.tf
@@ -1,31 +1,31 @@
-#data "aws_instances" "instances_list" {
-#  filter {
-#    name = "tag:Owner"
-#    values = [local.user]
-#  }
-#}
+data "aws_instances" "instances_list" {
+  filter {
+    name = "tag:Owner"
+    values = [local.user]
+  }
+}
 
-#output "instances_list" {
-#  value = "${data.aws_instances.instances_list}"
-#}
-
-data "aws_instance" "instance_details" {
-  for_each = toset(data.aws_instances.instances.ids)
-  instance_id = each.value
+output "instances_list" {
+  value = "${data.aws_instances.instances_list}"
 }
 
 output "public_ips" {
-  value = [for instance in data.aws_instance.instance_details : instance.public_ip]
+  value = [for instance in data.aws_instances.instances_list : instance.private_ip]
 }
 
 output "private_ips" {
-  value = [for instance in data.aws_instance.instance_details : instance.private_ip]
+  value = [for instance in data.aws_instances.instances_list : instance.private_ip]
 }
 
+#data "aws_instance" "instance_details" {
+#  for_each = toset(data.aws_instances.instances.ids)
+#  instance_id = each.value
+#}
+#
 #output "public_ips" {
-#  value = [for instance in data.aws_instances.instances_list : aws_instance.instance.private_ip]
+#  value = [for instance in data.aws_instance.instance_details : instance.public_ip]
 #}
 #
 #output "private_ips" {
-#  value = [for instance in data.aws_instances.instances_list : aws_instance.instance.private_ip]
+#  value = [for instance in data.aws_instance.instance_details : instance.private_ip]
 #}
