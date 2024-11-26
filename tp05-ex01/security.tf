fd926fdef43bb5467559595fd4062d40b5365832
0xPhantasia <evan.mestre02@gmail.com>
2024-11-26 14:32:50 +0100

    Setting port rules for efs

--- tp05-ex01/security.tf
+++ tp05-ex01/security.tf
@@ -64,7 +64,9 @@ resource "aws_vpc_security_group_egress_rule" "allow_nextcloud_all_ipv4_out" {
 resource "aws_vpc_security_group_egress_rule" "allow_nextcloud_efs_ipv4_out" {
   security_group_id = aws_security_group.nextcloud-sg.id
   cidr_ipv4         = "0.0.0.0/0"
-  ip_protocol       = "2049"
+  from_port   = 2049
+  ip_protocol = "tcp"
+  to_port     = 2049
 }
 
 ### EFS Security Group
@@ -85,13 +87,17 @@ resource "aws_security_group" "efs-sg" {
 resource "aws_vpc_security_group_ingress_rule" "allow_efs_nfs_ipv4_in" {
   security_group_id = aws_security_group.efs-sg.id
   cidr_ipv4         = "0.0.0.0/0"
-  ip_protocol       = "2049"
+  from_port   = 2049
+  ip_protocol = "tcp"
+  to_port     = 2049
 }
 
 resource "aws_vpc_security_group_egress_rule" "allow_efs_nfs_ipv4_out" {
   security_group_id = aws_security_group.efs-sg.id
   cidr_ipv4         = "0.0.0.0/0"
-  ip_protocol       = "2049"
+  from_port   = 2049
+  ip_protocol = "tcp"
+  to_port     = 2049
 }
 
 # COMMENTED OUT FOR C9 debugging
