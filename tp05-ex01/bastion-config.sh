9116d8290cb1a21e2278be8e6e98ac4636f62046
0xPhantasia <evan.mestre02@gmail.com>
2024-11-26 14:21:14 +0100

    Add dynamic bastion ip

--- tp05-ex01/bastion-config.sh
+++ tp05-ex01/bastion-config.sh
@@ -1,1 +1,3 @@
-scp -i ${path.module}/nextcloud-ssh.pem /home/ubuntu/nextcloud-ssh.pem ubuntu@${aws_instance.my_instances[0]}:/home/ubuntu/nextcloud-ssh.pem
+# Insecure, use for debugging only
+scp -i ${path.module}/nextcloud-ssh.pem /home/ubuntu/nextcloud-ssh.pem \
+ubuntu@${aws_instance.bastion.private_ip}:/home/ubuntu/nextcloud-ssh.pem
