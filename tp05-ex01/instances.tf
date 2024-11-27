fb8647eba496016f96c518d19b1612506ee0deba
0xPhantasia <evan.mestre02@gmail.com>
2024-11-27 17:25:05 +0100

    Dynamic EFS setup, removal of bash script use

--- tp05-ex01/instances.tf
+++ tp05-ex01/instances.tf
@@ -22,7 +22,25 @@ resource "aws_instance" "nextcloud" {
   subnet_id              = aws_subnet.private_subnets[keys(aws_subnet.private_subnets)[0]].id #Ugly
   vpc_security_group_ids = [aws_security_group.nextcloud-sg.id]
   key_name               = aws_key_pair.nextcloud.key_name
-  user_data = "${file("nextcloud-config.sh")}"
+#  user_data = "${file("nextcloud-config.sh")}"
+  user_data = <<-EOF
+    # Update system and ensure everything is up to date.
+    sudo apt update -y && sudo apt upgrade -y
+    
+    # Install requirement to mount EFS.
+    sudo apt install nfs-common
+    
+    # Create EFS mount directory.
+    sudo mkdir /mnt/efs
+    
+    # Mount EFS using the previously created directory. DNS is required.
+    sudo mount -t nfs4 -o nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2 \
+    ${aws_efs_file_system.nextcloud-efs.dns_name}:/ /mnt/efs
+    
+    # Enabling mount point persistence by appending config to /etc/fstab.
+    sudo echo ${aws_efs_file_system.nextcloud-efs.dns_name}:/ /mnt/efs nfs4 defaults,_netdev 0 0 >> /etc/fstab
+
+    EOF
 
   tags = {
     Name = "${local.name}-nextcloud"
--- tp05-ex01/nextcloud-config.sh
+++ tp05-ex01/nextcloud-config.sh
@@ -1,15 +1,1 @@
-# Update system and ensure everything is up to date.
-sudo apt update -y && sudo apt upgrade -y
 
-# Install requirement to mount EFS.
-sudo apt install nfs-common
-
-# Create EFS mount directory.
-sudo mkdir /mnt/efs
-
-# Mount EFS using the previously created directory. DNS is required.
-sudo mount -t nfs4 -o nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2 \
-fs-0537d2e1c72534748.efs.eu-north-1.amazonaws.com:/ /mnt/efs
-
-# Enabling mount point persistence by appending config to /etc/fstab.
-sudo echo fs-0537d2e1c72534748.efs.eu-north-1.amazonaws.com:/ /mnt/efs nfs4 defaults,_netdev 0 0 >> /etc/fstab
