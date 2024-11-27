19d6dfbf20a06c8e4e08c1104809330eeb99fb70
0xPhantasia <evan.mestre02@gmail.com>
2024-11-27 17:54:02 +0100

    Restrict access to C9 instances

--- tp05-ex01/security.tf
+++ tp05-ex01/security.tf
@@ -75,15 +75,6 @@ resource "aws_security_group" "efs-sg" {
   vpc_id      = aws_vpc.vpc.id
 }
 
-#resource "aws_vpc_security_group_ingress_rule" "allow_efs_nfs_ipv4_in" {
-#  security_group_id = aws_security_group.efs-sg.id
-#  cidr_ipv4         = "${aws_instance.nextcloud.private_ip}/32"
-#  from_port         = 2049
-#  ip_protocol       = "tcp"
-#  to_port           = 2049
-#}
-
-
 resource "aws_vpc_security_group_ingress_rule" "allow_efs_nfs_ipv4_in" {
   security_group_id = aws_security_group.efs-sg.id
   cidr_ipv4         = "${aws_instance.nextcloud.private_ip}/32"
@@ -100,34 +91,34 @@ resource "aws_vpc_security_group_egress_rule" "allow_efs_nfs_ipv4_out" {
   to_port     = 2049
 }
 
-# COMMENTED OUT FOR C9 debugging
-##ACL restraining access to ressources from C9 instances
-#resource "aws_network_acl" "acl" {
-#  vpc_id = aws_vpc.vpc.id
-#  
-#  ingress {
-#    protocol   = "tcp"
-#    rule_no    = 50
-#    action     = "deny"
-#    cidr_block = "13.38.91.0/24"
-#    from_port  = 22
-#    to_port    = 22
-#  }
-#}
-#
-##Applying ACL to all VPC subnets
-#resource "aws_network_acl_association" "acl_association_private_subnets" {
-#  network_acl_id = aws_network_acl.acl.id
-#  for_each       = aws_subnet.private_subnets
-#  subnet_id      = each.value.id
-#}
+#COMMENT OUT FOR C9 debugging
+#ACL restraining access to ressources from C9 instances
+resource "aws_network_acl" "acl" {
+  vpc_id = aws_vpc.vpc.id
+  
+  ingress {
+    protocol   = "tcp"
+    rule_no    = 50
+    action     = "deny"
+    cidr_block = "13.38.91.0/24"
+    from_port  = 22
+    to_port    = 22
+  }
+}
 
-##Applying ACL to all VPC subnets
-#resource "aws_network_acl_association" "acl_association_public_subnets" {
-#  network_acl_id = aws_network_acl.acl.id
-#  for_each       = aws_subnet.public_subnets
-#  subnet_id      = each.value.id
-#}
+#Applying ACL to all VPC private subnets
+resource "aws_network_acl_association" "acl_association_private_subnets" {
+  network_acl_id = aws_network_acl.acl.id
+  for_each       = aws_subnet.private_subnets
+  subnet_id      = each.value.id
+}
+
+#Applying ACL to all VPC public subnets
+resource "aws_network_acl_association" "acl_association_public_subnets" {
+  network_acl_id = aws_network_acl.acl.id
+  for_each       = aws_subnet.public_subnets
+  subnet_id      = each.value.id
+}
 
 
 
