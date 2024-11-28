b671af291f1ecea9cbbd16bc351133f4d7fa19c9
0xPhantasia <evan.mestre02@gmail.com>
2024-11-28 12:13:05 +0100

    ALB healthcheck and forwarding

--- tp07-ex01/network.tf
+++ tp07-ex01/network.tf
@@ -87,6 +87,7 @@ resource "aws_nat_gateway" "nat_gateway" {
   depends_on = [aws_internet_gateway.internet_gateway]
 }
 
+#Creating Application Load Balancer
 resource "aws_lb" "nextcloud-alb" {
   internal           = false
   load_balancer_type = "application"
@@ -94,3 +95,41 @@ resource "aws_lb" "nextcloud-alb" {
   subnets            = [for subnet in aws_subnet.public_subnets : subnet.id]
   enable_deletion_protection = false
 }
+
+#Setting ALB target group
+resource "aws_lb_target_group" "nextcloud-alb" {
+  port        = 80
+  protocol    = "HTTP"
+  vpc_id      = aws_vpc.vpc.id
+
+  target_group_health {
+   dns_failover {
+     minimum_healthy_targets_count      = "1"
+     minimum_healthy_targets_percentage = "off"
+   }
+   unhealthy_state_routing {
+     minimum_healthy_targets_count      = "1"
+     minimum_healthy_targets_percentage = "off"
+   }
+  }
+}
+
+#Attach Nextcloud instance to nextcloud ALB target group
+resource "aws_lb_target_group_attachment" "nextcloud-alb" {
+  target_group_arn = aws_lb_target_group.nextcloud-alb.arn
+  target_id        = aws_instance.nextcloud.id
+  port             = 80
+}
+
+#Config ALB listener
+resource "aws_lb_listener" "nextcloud-alb-listener" {
+  load_balancer_arn = aws_lb.nextcloud-alb.arn
+  port              = "80"
+  protocol          = "HTTP"
+
+  default_action {
+    type = "forward"
+    target_group_arn = aws_lb_target_group.nextcloud-alb.arn
+  }
+}
+
