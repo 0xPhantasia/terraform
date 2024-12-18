f9b4e7ab929f4b3cbab41fb3f9b2549696e3487e
0xPhantasia <evan.mestre02@gmail.com>
2024-12-19 00:12:22 +0100

    Uncommenting ACL

--- tp08-ex01/security.tf
+++ tp08-ex01/security.tf
@@ -119,35 +119,35 @@ resource "aws_vpc_security_group_egress_rule" "allow_alb_all_ipv4_out" {
 }
 
 
-##ACL restraining access to ressources from C9 instances
-#resource "aws_network_acl" "acl" {
-#  vpc_id = aws_vpc.vpc.id
-#  
-#  ingress {
-#    protocol   = "tcp"
-#    rule_no    = 50
-#    action     = "deny"
-#    cidr_block = "13.38.91.0/24"
-#    from_port  = 22
-#    to_port    = 22
-#  }
-#}
+#ACL restraining access to ressources from C9 instances
+resource "aws_network_acl" "acl" {
+  vpc_id = aws_vpc.vpc.id
+  
+  ingress {
+    protocol   = "tcp"
+    rule_no    = 50
+    action     = "deny"
+    cidr_block = "13.38.91.0/24"
+    from_port  = 22
+    to_port    = 22
+  }
+}
 
 #COMMENT OUT FOR C9 debugging
 #Applying ACL to all VPC private subnets
-#resource "aws_network_acl_association" "acl_association_private_subnets" {
-#  network_acl_id = aws_network_acl.acl.id
-#  for_each       = aws_subnet.private_subnets
-#  subnet_id      = each.value.id
-#}
-#
-##COMMENT OUT FOR C9 debugging
-##Applying ACL to all VPC public subnets
-#resource "aws_network_acl_association" "acl_association_public_subnets" {
-#  network_acl_id = aws_network_acl.acl.id
-#  for_each       = aws_subnet.public_subnets
-#  subnet_id      = each.value.id
-#}
+resource "aws_network_acl_association" "acl_association_private_subnets" {
+  network_acl_id = aws_network_acl.acl.id
+  for_each       = aws_subnet.private_subnets
+  subnet_id      = each.value.id
+}
+
+#COMMENT OUT FOR C9 debugging
+#Applying ACL to all VPC public subnets
+resource "aws_network_acl_association" "acl_association_public_subnets" {
+  network_acl_id = aws_network_acl.acl.id
+  for_each       = aws_subnet.public_subnets
+  subnet_id      = each.value.id
+}
 
 
 
