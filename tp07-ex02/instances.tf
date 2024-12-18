a5860261f6f2bac9dec3de7b1adc85cf25e8b8e9
0xPhantasia <evan.mestre02@gmail.com>
2024-12-18 22:34:04 +0100

    We do be trying some lil ASG n stuff

--- tp07-ex02/instances.tf
+++ tp07-ex02/instances.tf
@@ -1,34 +1,90 @@
-# Retrieve the latest Ubuntu AMD64 AMI
-# Retrieve the latest Ubuntu 22.04 LTS AMD64 AMI
-data "aws_ami" "ubuntu_latest" {
+## Retrieve the latest Ubuntu AMD64 AMI
+## Retrieve the latest Ubuntu 22.04 LTS AMD64 AMI
+#data "aws_ami" "ubuntu_latest" {
+#  most_recent = true
+#
+#  owners = ["099720109477"] # Canonical's AWS Account ID
+#
+#  filter {
+#    name   = "name"
+#    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
+#  }
+#
+#  filter {
+#    name   = "virtualization-type"
+#    values = ["hvm"]
+#  }
+#}
+
+# Recover custom made AMI
+data "aws_ami" "nextcloud_custom" {
   most_recent = true
 
-  owners = ["099720109477"] # Canonical's AWS Account ID
+  owners = ["self"]
 
   filter {
     name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
+    values = ["${local.user}-${local.tp}-nextcloud*"]
   }
 
   filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
+    name   = "state"
+    values = ["available"]
   }
 }
 
-#Create Nextcloud Instance
-resource "aws_instance" "nextcloud" {
-  ami                    = data.aws_ami.ubuntu_latest.id
-  instance_type          = "t3.micro"
-  subnet_id              = values(aws_subnet.private_subnets)[0].id
+
+##Create Nextcloud Instance
+#resource "aws_instance" "nextcloud" {
+#  ami                    = data.aws_ami.ubuntu_latest.id
+#  instance_type          = "t3.micro"
+#  subnet_id              = values(aws_subnet.private_subnets)[0].id
+#  vpc_security_group_ids = [aws_security_group.nextcloud-sg.id]
+#  key_name               = aws_key_pair.nextcloud.key_name
+#  user_data              = local.nextcloud_userdata
+#
+#  tags = {
+#    Name = "${local.name}-nextcloud"
+#  }
+#}
+
+resource "aws_launch_template" "nextcloud" {
+  name_prefix   = "${local.name}-nextcloud-lt"
+  image_id      = data.aws_ami.nextcloud_custom.id
+  instance_type = "t3.micro"
+  key_name      = aws_key_pair.nextcloud.key_name
   vpc_security_group_ids = [aws_security_group.nextcloud-sg.id]
-  key_name               = aws_key_pair.nextcloud.key_name
-  user_data              = local.nextcloud_userdata
+  
+  tag_specifications {
+    resource_type = "instance"
+    tags = {
+      Name  = "${local.name}-nextcloud-instance"
+      Owner = local.user
+    }
+  }
+
+  tag_specifications {
+    resource_type = "volume"
+    tags = {
+      Name  = "${local.name}-nextcloud-volume"
+      Owner = local.user
+    }
+  }
+
+  tag_specifications {
+    resource_type = "network-interface"
+    tags = {
+      Name  = "${local.name}-nextcloud-nic"
+      Owner = local.user
+    }
+  }
 
   tags = {
-    Name = "${local.name}-nextcloud"
+    Name  = "${local.name}-nextcloud-lt"
+    Owner = local.user
   }
 }
+
 
 #Create Bastion Instance
 resource "aws_instance" "bastion" {
