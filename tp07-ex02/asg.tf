cf696b030fbd1878b24d1dd6d901746d78ab4367
0xPhantasia <evan.mestre02@gmail.com>
2024-12-18 23:36:03 +0100

    Uncomment ASG and custom AMI config.

--- tp07-ex02/asg.tf
+++ tp07-ex02/asg.tf
@@ -1,33 +1,32 @@
-#resource "aws_autoscaling_group" "nextcloud" {
-#  name                      = "${local.name}-nextcloud"
-#  desired_capacity          = 1
-#  min_size                  = 1
-#  max_size                  = 1
-#  health_check_type         = "ELB"
-#  health_check_grace_period = 300
-#
-#  # Utiliser les subnets privés sur les 3 AZ
-#  vpc_zone_identifier = values(aws_subnet.private_subnets)[*].id
-#
-#  # Utiliser le Launch Template nouvellement créé
-#  launch_template {
-#    id      = aws_launch_template.nextcloud.id
-#    version = "$Latest"
-#  }
-#
-#  # Attache l'ASG au target group de l'ALB
-#  target_group_arns = [aws_lb_target_group.nextcloud.arn]
-#
-#  # Permet une mise à jour propre (évite de détruire avant de créer une nouvelle instance)
-#  lifecycle {
-#    create_before_destroy = true
-#  }
-#
-#  # Tags de l'ASG
-#  tag {
-#    key                 = "Owner"
-#    value               = local.user
-#    propagate_at_launch = false
-#  }
-#}
-#
+resource "aws_autoscaling_group" "nextcloud" {
+  name                      = "${local.name}-nextcloud"
+  desired_capacity          = 1
+  min_size                  = 1
+  max_size                  = 1
+  health_check_type         = "ELB"
+  health_check_grace_period = 300
+
+  # Utiliser les subnets privés sur les 3 AZ
+  vpc_zone_identifier = values(aws_subnet.private_subnets)[*].id
+
+  # Utiliser le Launch Template nouvellement créé
+  launch_template {
+    id      = aws_launch_template.nextcloud.id
+    version = "$Latest"
+  }
+
+  # Attache l'ASG au target group de l'ALB
+  target_group_arns = [aws_lb_target_group.nextcloud.arn]
+
+  # Permet une mise à jour propre (évite de détruire avant de créer une nouvelle instance)
+  lifecycle {
+    create_before_destroy = true
+  }
+
+  # Tags de l'ASG
+  tag {
+    key                 = "Owner"
+    value               = local.user
+    propagate_at_launch = false
+  }
+}
--- tp07-ex02/instances.tf
+++ tp07-ex02/instances.tf
@@ -1,89 +1,89 @@
 # Retrieve the latest Ubuntu AMD64 AMI
 # Retrieve the latest Ubuntu 22.04 LTS AMD64 AMI
-data "aws_ami" "ubuntu_latest" {
-  most_recent = true
-
-  owners = ["099720109477"] # Canonical's AWS Account ID
-
-  filter {
-    name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
-  }
-
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-}
-
-## Recover custom made AMI
-#data "aws_ami" "nextcloud_custom" {
+#data "aws_ami" "ubuntu_latest" {
 #  most_recent = true
 #
-#  owners = ["self"]
+#  owners = ["099720109477"] # Canonical's AWS Account ID
 #
 #  filter {
 #    name   = "name"
-#    values = ["${local.user}-${local.tp}-nextcloud*"]
+#    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
 #  }
 #
 #  filter {
-#    name   = "state"
-#    values = ["available"]
+#    name   = "virtualization-type"
+#    values = ["hvm"]
 #  }
 #}
 
+# Recover custom made AMI
+data "aws_ami" "nextcloud_custom" {
+  most_recent = true
 
-#Create Nextcloud Instance
-resource "aws_instance" "nextcloud" {
-  ami                    = data.aws_ami.ubuntu_latest.id
-  instance_type          = "t3.micro"
-  subnet_id              = values(aws_subnet.private_subnets)[0].id
-  vpc_security_group_ids = [aws_security_group.nextcloud-sg.id]
-  key_name               = aws_key_pair.nextcloud.key_name
-  user_data              = local.nextcloud_userdata
+  owners = ["self"]
 
-  tags = {
-    Name = "${local.name}-nextcloud"
+  filter {
+    name   = "name"
+    values = ["${local.user}-${local.tp}-nextcloud*"]
+  }
+
+  filter {
+    name   = "state"
+    values = ["available"]
   }
 }
 
-#resource "aws_launch_template" "nextcloud" {
-#  name_prefix   = "${local.name}-nextcloud-lt"
-#  image_id      = data.aws_ami.nextcloud_custom.id
-#  instance_type = "t3.micro"
-#  key_name      = aws_key_pair.nextcloud.key_name
+
+##Create Nextcloud Instance
+#resource "aws_instance" "nextcloud" {
+#  ami                    = data.aws_ami.ubuntu_latest.id
+#  instance_type          = "t3.micro"
+#  subnet_id              = values(aws_subnet.private_subnets)[0].id
 #  vpc_security_group_ids = [aws_security_group.nextcloud-sg.id]
-#
-#  tag_specifications {
-#    resource_type = "instance"
-#    tags = {
-#      Name  = "${local.name}-nextcloud-instance"
-#      Owner = local.user
-#    }
-#  }
-#
-#  tag_specifications {
-#    resource_type = "volume"
-#    tags = {
-#      Name  = "${local.name}-nextcloud-volume"
-#      Owner = local.user
-#    }
-#  }
-#
-#  tag_specifications {
-#    resource_type = "network-interface"
-#    tags = {
-#      Name  = "${local.name}-nextcloud-nic"
-#      Owner = local.user
-#    }
-#  }
+#  key_name               = aws_key_pair.nextcloud.key_name
+#  user_data              = local.nextcloud_userdata
 #
 #  tags = {
-#    Name  = "${local.name}-nextcloud-lt"
-#    Owner = local.user
+#    Name = "${local.name}-nextcloud"
 #  }
 #}
+
+resource "aws_launch_template" "nextcloud" {
+  name_prefix   = "${local.name}-nextcloud-lt"
+  image_id      = data.aws_ami.nextcloud_custom.id
+  instance_type = "t3.micro"
+  key_name      = aws_key_pair.nextcloud.key_name
+  vpc_security_group_ids = [aws_security_group.nextcloud-sg.id]
+
+  tag_specifications {
+    resource_type = "instance"
+    tags = {
+      Name  = "${local.name}-nextcloud-instance"
+      Owner = local.user
+    }
+  }
+
+  tag_specifications {
+    resource_type = "volume"
+    tags = {
+      Name  = "${local.name}-nextcloud-volume"
+      Owner = local.user
+    }
+  }
+
+  tag_specifications {
+    resource_type = "network-interface"
+    tags = {
+      Name  = "${local.name}-nextcloud-nic"
+      Owner = local.user
+    }
+  }
+
+  tags = {
+    Name  = "${local.name}-nextcloud-lt"
+    Owner = local.user
+  }
+}
 
 
 #Create Bastion Instance
